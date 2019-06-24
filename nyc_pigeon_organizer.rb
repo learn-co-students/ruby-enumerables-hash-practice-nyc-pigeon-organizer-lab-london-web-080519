0c8b4a033f2a5a0243de6b28b5be5491e67c0f6a
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-24 20:23:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #create an empty hash to store data
+ my_pigeons = {}
+ #puts "keys: #{data.keys} / values: #{data.values}"
+ data.values.each do |attribute|  #look inside the first structure, color/gender/lives
+   attribute.each do | value, array | #look inside the next structure, values(options) for attributes
+      array.each do |name| #third, containing the names is an array. get the names out of the arrays
+           my_pigeons[name] =
+            {
+              color: nil,
+              gender: nil,
+              lives: nil
+            }
+       end
+    end
+
+ end
+my_pigeons
 end

b9c597aa289bb484e22c18dd158b6d3713818dab
Chris Hodge <chrishodge87@gmail.com>
2019-07-22 09:19:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+output = {}
+
+    data.each do |attri, val|
+    val.each do |name, array|
+    array.each do |pigeon|
+
+            if !output[pigeon]
+                    output[pigeon] =
+                    {color: [], gender: [], lives: []}
+            end
+            if output[pigeon]
+                    output[pigeon][attri] << "#{name}"
+            end
+      end
+      end
+    end
+    p output
 end

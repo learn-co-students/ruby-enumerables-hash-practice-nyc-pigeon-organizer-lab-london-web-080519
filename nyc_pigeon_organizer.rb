c20a63cac3e1c6b16635b4a846727dbad4450108
Aude Faucheux <aude53@gmail.com>
2019-07-27 18:32:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key, value_array|
+    value_array.each do |value, name_array|
+      name_array.each do |names|
+        if !new_hash[names]
+          new_hash[names] = {}
+        end
+
+        if !new_hash[names][key]
+          new_hash[names][key] = []
+        end
+
+        new_hash[names][key].push(value.to_s)
+      end
+    end
+  end
+  return new_hash
 end
